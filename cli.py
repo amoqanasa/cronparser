2aea1dfcc4e8a7382bdcd804a1b06eba885cb795
Ahmad Moqanasa <ahmad.maqnassah@gmail.com>
2019-10-10 23:46:45 +0200

    fix cli

--- cli.py
+++ cli.py
@@ -1,12 +1,15 @@
 import sys
 from cronparser import Parser
 
+def print_numlist(numlist):
+    return " ".join([str(num) for num in numlist])
+
 def pretty_print(entry):
-    print("minute       {}".format(entry['minute']))
-    print("hour         {}".format(entry['hour']))
-    print("day of month {}".format(entry['day of month']))
-    print("day of week  {}".format(entry['day of week']))
-    print("month        {}".format(entry['month']))
+    print("minute       {}".format(print_numlist(entry['minute'])))
+    print("hour         {}".format(print_numlist(entry['hour'])))
+    print("day of month {}".format(print_numlist(entry['day of month'])))
+    print("month        {}".format(print_numlist(entry['month'])))
+    print("day of week  {}".format(print_numlist(entry['day of week'])))
     print("command      {}".format(entry['command']))
 
 args = sys.argv[1:]
